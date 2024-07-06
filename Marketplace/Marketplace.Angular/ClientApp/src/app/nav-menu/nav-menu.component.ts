e82a7136ace45d3c464ed108dcd87b21fd1a37e8
Tentrun <tentrun@gmail.com>
2024-07-06 18:10:22 +0300

    Обновление текущей вкладки в нав баре, если перешли по ссылке

--- Marketplace/Marketplace.Angular/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Marketplace/Marketplace.Angular/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject} from '@angular/core';
 import {TuiAlertService} from "@taiga-ui/core";
-import {Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav-menu',
@@ -11,12 +11,24 @@ export class NavMenuComponent {
   isExpanded = false;
 
   activeItemIndex = 0;
-
   constructor(
     @Inject(TuiAlertService)
     private readonly alerts: TuiAlertService, private readonly router: Router
   ) {
 
+  }
+
+  ngOnInit(){
+    //Получаем текущий индекс, если перешли не из nav menu
+    //Можно переделать на константы, но, надо провайдить в app routes
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        switch (e.url){
+          case "/infrastructure-status":
+            this.activeItemIndex = 4;
+        }
+      }
+    });
   }
 
   onClick(item: string): void {
